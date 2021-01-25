23a16a586391afab782cff3f289775afa4347dfc
Sebastian Ariza <sebas.camilo012@hotmail.com>
2021-01-25 01:19:57 -0500

    Inicializando el contenido

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,34 @@
+import kotlin.Int as Int
+
+fun MakeRecipes(){
+    print("::Creando Receta::")
+}
+
+fun ViewRecipes(){
+    print("::Mis Recetas::")
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val option = '1'..'3'
+
+    val MENU = """
+        Selecciona la opcion Deseada
+        1. Hacer una receta
+        2. Ver mis recetas
+        3. Salir
+    """.trimIndent()
+
+    var response: Int
+
+    do {
+        println(MENU)
+        response = readLine()!!.toInt()
+        when(response){
+            1 -> MakeRecipes()
+            2 -> ViewRecipes()
+        }
+        break
+    }while (response != 3)
+
 }
